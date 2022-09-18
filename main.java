d03b952cd35dae6a23bbd7eb1b64b1931eb373ea
alxpino <113858490+alxpino@users.noreply.github.com>
2022-09-18 17:08:40 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,25 @@
 Public class Main{
-
+  
+  public static void main(String [] args){
+    suma(1, 2, 3);
+    coche micoche = new coche;
+    micoche.masPuertas();
+    System.out.printLn(miCoche.puertas);
+  }
+  
+  
+  
+  public static suma(int a, int b, int c){
+    return a + b + c;
+  }
+    
 }
+
+ class coche{
+    public int puertas = 4;
+    
+    public void masPuertas(){
+       this.puertas++;
+    }
+    
+  }
